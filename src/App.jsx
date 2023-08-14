e154f3d6f24581696f96c83e1189eccc7adffee6
ranepaarth <2019paarth.rane@ves.ac.in>
2023-08-14 21:00:04 +0530

    modified final

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
+import Loading from "./components/Loading";
 
 function App() {
-  const [posts,setPosts] = useState([]);
-  const [loading,setLoading] = useState(false);
-  const [currentPage,setCurrentPage] = useState(1);
-  const [postsPerPage,setPostsPerPage] = useState(15);
-  
-  useEffect(()=> {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
       setPosts(res.data);
       setLoading(false);
-    }
+    };
 
     fetchPosts();
-  },[]);
+  }, []);
 
-  console.log(posts);
+  // console.log(posts);
 
-    //Get Current Posts
+  //Get Current Posts
 
-    const indexOfLastPost = currentPage * postsPerPage;  //eg. currentPage=2 PostsPerPage=10 then indexOfLastPost = (2*10) =20
-    const indexOfFirstPost = indexOfLastPost - postsPerPage; //eg. indexOfLastPost=20 postsPerPage=10 then indexOfFirstPost =(20-10)= 10 
-    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const indexOfLastPost = currentPage * postsPerPage; //eg. currentPage=2 PostsPerPage=10 then indexOfLastPost = (2*10) =20
+  const indexOfFirstPost = indexOfLastPost - postsPerPage; //eg. indexOfLastPost=20 postsPerPage=10 then indexOfFirstPost =(20-10)= 10
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
-  return (<div className="flex justify-center flex-col items-center"><h1 className="text-2xl font-[400] bg-teal-300 mt-4 p-5 rounded-xl font-Montserrat shadow-md">My Blog</h1><Posts posts={currentPosts} loading={loading}/></div>);
+
+  //change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+
+  // if (loading) {
+  //   return <Loading />;
+  // }
+
+  return (
+    <div className="flex justify-center flex-col items-center font-Montserrat">
+      <h1 className="text-2xl font-[400] bg-teal-300 mt-4 p-5 rounded-xl font-Montserrat shadow-md">
+        Paginate
+      </h1>
+      <>{loading? <div className="h-[60dvh] flex items-center justify-center"><Loading /></div>: <Posts posts={currentPosts}/>}</>
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import Loading from "./Loading";
 
-const Posts = ({ posts, loading }) => {
-  if (loading) {
-    return <h2>Loading....</h2>;
-  }
+const Posts = ({ posts}) => {
+
 
 
 
   return (
-    <section className="font-Montserrat my-10 p-8 rounded-3xl bg-teal-100">
+    <section className="font-Montserrat text-sm w-fit m-8 py-3 px-5 rounded-3xl bg-teal-100 shadow-lg md:text-xl">
       {posts.map((post) => {
-        return (<article key={post.id} className="py-2 capitalize">
+        return (<article key={post.id} className="py-[5px] capitalize">
           {post.id}. {post.title}
         </article>);
       })}
