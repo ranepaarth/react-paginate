51663ed756789ce33feb8ec58a26ea2d1bff4ecf
ranepaarth <2019paarth.rane@ves.ac.in>
2023-08-19 12:39:14 +0530

    small change

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,7 +4,7 @@ import Loading from "./Loading";
 const Posts = ({ posts}) => {
 
   return (
-    <section className="font-Montserrat text-sm w-fit m-8 py-3 px-5 rounded-3xl bg-teal-100 shadow-lg md:text-xl">
+    <section className="font-Montserrat text-sm w-fit mt-4 py-3 px-5 rounded-3xl bg-teal-100 shadow-lg md:text-xl mb-24 mx-5">
       {posts.map((post) => {
         return (<article key={post.id} className="py-[5px] capitalize">
           {post.id}. {post.title}
