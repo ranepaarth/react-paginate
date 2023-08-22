0c1fc00ed5493b8cd38b65a801a239f9cadc6877
ranepaarth <2019paarth.rane@ves.ac.in>
2023-08-22 21:28:55 +0530

    theme2: done

--- src/components/Theme/Theme2.jsx
+++ src/components/Theme/Theme2.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import Loading from "../Loading";
 
 const Theme2 = () => {
   const [products, setProducts] = useState([]);
 
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
-    const res = await fetch("https://dummyjson.com/products?limit=100");
+    setLoading(true);
+    const res = await fetch("https://dummyjson.com/products?limit=90");
     const data = await res.json();
-
+    setLoading(false);
     // console.log(data);
     //optional chaining  same as  (data && data.products)
     if (data?.products) {
@@ -18,41 +20,66 @@ const Theme2 = () => {
   };
 
   // console.log(products);
-
   useEffect(() => {
     fetchData();
   }, []);
 
-  return (
-    <div className="bg-neutral-100 h-[90vh] min-w-full">
-      {products.length > 0 && (
-        <div className="m-[20px] grid gap-[20px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {/*we have used .slice(START,END) method of arrays which slices our array and displays data within the range specified including the START INDEX and excluding the END INDEX */}
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 &&
+      selectedPage !== page
+    )
+      setPage(selectedPage);
+  };
 
-          {products.slice(page * 10 - 10, page * 10).map((product) => {
-            return (
-              <span
-                className="h-[250px] p-[20px] bg-neutral-50 text-center rounded-lg cursor-pointer border border-neutral-400 shadow-lg"
-                key={product.id}
-              >
-                <img
-                  src={product.thumbnail}
-                  alt={product.title}
-                  className="w-[100%] h-[95%] object-cover mb-[3px]"
-                />
-                <span>{product.title}</span>
-              </span>
-            );
-          })}
-        </div>
-      )}
-      <div>
-        <span>Prev</span>
-        <span></span>
-        <span>Next</span>
+  if (loading) {
+    return (
+      <div className="h-[80dvh] flex justify-center items-center">
+        <Loading />
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="bg-neutral-100 h-[90vh] min-w-full font-Montserrat">
+        {products.length > 0 && (
+          <div className="m-[20px] grid gap-[20px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {/*we have used .slice(START,END) method of arrays which slices our array and displays data within the range specified including the START INDEX and excluding the END INDEX */}
+
+            {products.slice(page * 9 - 9, page * 9).map((product) => {
+              return (
+                <span
+                  className="h-[250px] p-[20px] bg-teal-200 text-center rounded-lg cursor-pointer border border-neutral-400 shadow-lg"
+                  key={product.id}
+                >
+                  <img
+                    src={product.thumbnail}
+                    alt={product.title}
+                    className="w-[100%] h-[90%] object-cover mb-[8px]"
+                  />
+                  <span>{product.title}</span>
+                </span>
+              );
+            })}
+          </div>
+        )}
+
+        <div className="flex justify-center items-center mb-5 py-5 px-10 gap-x-6 text-xl cursor-pointer" >
+          <span onClick={() => selectPageHandler(page - 1)} className={page>1 ? "bg-teal-300 p-3 rounded-xl": "hidden"}>Prev</span>
+          <span className="flex justify-center items-center gap-x-5">
+            {[...Array(products.length / 10)].map((_, index) => {
+              return (
+                <span onClick={() => selectPageHandler(index + 1)} className='bg-teal-300 p-3 rounded-full w-10 h-10 flex items-center justify-center'>
+                  {index + 1}
+                </span>
+              );
+            })}
+          </span>
+          <span onClick={() => selectPageHandler(page + 1)}className={page<products.length/10 ? "bg-teal-300 p-3 rounded-xl": "hidden"}>Next</span>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Theme2;
